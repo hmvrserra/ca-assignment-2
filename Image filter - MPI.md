86a824b3c2e4dff69b3e51b3b1ff9ce3b98a9b7b
hmvrserra <hugomrserra@gmail.com>
2026-05-27 23:32:13 +0100

    feat: initialize MPI image filtering project with boilerplate code and assignment documentation

---
+++ Image filter - MPI.md
@@ -0,0 +1,75 @@
+# Advanced Computing
+## Project 2 - MPI
+
+### Introduction
+
+The project focuses on two common tasks in image processing, namely image filtering and histogram generation. We will be using "grayscale" images, which specify only light intensity using 1 byte per pixel, ranging from black (0) to white (255).
+
+The filtering is performed with a standard technique that performs a convolution between a kernel (a small 3×3 matrix in our case) and the image. Each pixel in the output image is a function of the nearby pixels (including itself) using the kernel values as weights. The code does 32 passes of the so called "Gaussian blur" kernel. The histogram calculation computes the number of pixels in the image that have a particular light intensity. This is achieved by looping through all pixels in the image and accumulating the histogram bin corresponding to the intensity of each pixel. The provided `filter.c` code implements these tasks in the `gauss()` and `hist()` functions.
+
+
+Note that this is a group assignment. Students must work in groups of two elements. Groups may be formed by any two students enrolled in the Advanced Computing course.
+
+### Goals
+
+The goal of this project is to parallelize the provided serial version of the image processing code using **MPI**, and to analyze the performance of the parallel program by measuring the execution time for an increasing number of processes.
+
+### Implementation
+
+The parallel MPI program will operate as follows:
+
++ Rank 0 will read the image data, and broadcast the image dimensions to all ranks. The paralelization will split the image vertically over the available processes. The local image buffer on each process includes two additional rows, one at the bottom, and one at the top, to work as guard cells. 
++ The histogram computation will operate on the local image data, excluding the guard cells. Once complete, a parallel reduction must be applied to return the value on rank 0, which will then save the output histogram file.
++ Before filtering, all processes must exchange guard cells values with their neighbors. Note that rank 0 does not have a lower neighbor, and the last rank does not have an upper neighbor. Once the exchange is complete, filtering is applied to local image data, once again excluding guard cells. The process will be repeated `level` times.
++ Once the filtering is complete, the program will gather the image data (excluding guard cells) on rank 0, which will then save the final image.
+
+Use the provided template to write your code. The template already includes the file read/write, histogram and filter routines, as well as MPI initialization and cleanup. It also includes placeholders for you to place your code. __Do not change the structure of the code__.
+
+Start by downloading the project file available in the e-learning (Moodle) platform and decompress it into the `labs` folder inside your Docker course image. The following files are provided:
+
++ `filter.c` - Template for the image processing code;
++ `dog8.pgm` - Small image for testing purposes;
++ `alex8.pgm` - Large image for performance measuring;
++ `filter.ipynb` - Simple jupyter notebook for visualizing the images and histogram. 
+
+Once complete, the `filter.c` program expects a single command line parameter with the name of the image file that is to be processed, e.g.:
+
+```bash
+$ mpiexec -np 4 ./filter dog8.ppm
+```
+
+The program will always save the results in the same files, namely `filter.pgm` (filtered image), and `histogram.csv` (histogram). If you have python (and required dependencies) installed on your system, you may use the supplied `filter.ipynb` to visualize your results.
+
+#### 1. Add code to broadcast the image dimensions to all processes
+
+Rank 0 must broadcast the image dimensions (2 integers) to all processes. Use `MPI_Bcast()`.
+
+#### 2. Add code to split the image data over all processes
+
+Use `MPI_Scatter()` for this purpose. Data size will be `nx * ny` of type `MPI_BYTE`. You should place the received data in the `local_img[]` array at position `nx`, to leave an empty row at the bottom of the local image that will serve as a guard cell.
+
+#### 3. Add code to gather the image data from all processes on rank 0
+
+Use `MPI_Gather()` for this purpose. The call should implement the reverse of the operation you performed in step 2. Note that the placeholder is closer to the end of the code.
+
+_Hint_: Once you have completed this step you should test the code. If you comment the `gauss()` call, the output file should be identical to the input file. 
+
+#### 4. Compute the global histogram on rank 0
+
+Use `MPI_Reduce()` for this purpose. The call should add the values of the `histogram[]` array available on all processes, and return the value on the `global[]` array on rank 0.
+
+Test the parallel version with the `dog.pgm` file, and check for correctness.
+
+#### 5. Add code to exchange edge values of the `local_img[]` array
+
+The communication should copy the values on the lower and upper edges of `local_img[]` in a given process, to the corresponding guard cell rows of the lower and upper neighbors. Keep in mind that rank 0 does not have a lower neighbor, and the last rank does not have an upper neighbor.
+
+You may use whatever point to point communication routines you see fit.
+
+### Documents to submit
+
+The submission of the project must include both the parallel version of the serial code and a short report (maximum 2 pages) discussing the parallelization strategy followed for each of the image processing tasks, and discussing the speedup obtained from the serial version by the parallel code for the image filtering task. Remember to include a brief description of the hardware used, including the processor type and number of cores.
+
+Both the report and the source file must clearly identify the two members of the group, including name and student number. Please submit both files to the e-learning (Moodle) platform in the section made available for this purpose (Avaliação → Projecto 2 - MPI). 
+
+__Do not submit the test images__.
---
+++ filter.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h>
+#include <string.h>
+
+/**
+ * @brief Reads PGM (portable GrayMap) image
+ * 
+ * The routine will allocate (malloc) the `img` buffer, this must be 
+ * deallocated explicitly once it is no longer necessary
+ * 
+ * @note  Only bit depths of up to 255 are supported
+ * 
+ * @param img       Output image
+ * @param dims      Image dimensions
+ * @param fname     File name
+ * @return int 
+ */
+int read_pgm( uint8_t ** img, int dims[], char * fname ) {
+    FILE *fp = fopen( fname, "r");
+    if ( !fp ) {
+        perror("Unable to open file\n" );
+        return -1;
+    }
+
+    char buffer[1024];
+
+    // Check magic
+    fread( buffer, 1, 3, fp );
+    if ( buffer[0] != 'P' || buffer[1] != '5' || buffer[2] != '\n') {
+        fprintf(stderr, "Unsupported file format\n");
+        return -1;
+    }
+
+    // Skip comment, if any
+    char comment = 0;
+    do {
+        fread( &comment, 1, 1, fp );
+        if ( comment == '#' ) {
+            fscanf( fp, "%[^\n]s", buffer );
+            // printf("%s: %s\n", fname, buffer );
+        } else {
+            fseek( fp, -1, SEEK_CUR );
+        }
+    } while ( comment == '#' );
+
+    int depth;
+    fscanf( fp, "%d %d %d", &dims[0], &dims[1], &depth );
+
+    if ( depth > 255 ) {
+        fprintf(stderr, "Unsupported bit depth (%d)\n", depth);
+        fclose(fp);
+        return -1;
+    }
+
+    *img = malloc( dims[0] * dims[1] );
+    if ( *img == NULL ) {
+        fprintf(stderr, "Unable to allocate memory for file\n");
+        fclose(fp);
+        return -1;
+    }
+
+    fread( *img, 1, dims[0] * dims[1], fp );
+    fclose(fp);
+
+    return 0;
+}
+
+/**
+ * @brief Write PGM (portable GrayMap) image
+ * 
+ * @note Only bit depths of 8 bits are supported
+ * 
+ * @param image     Image data
+ * @param dims      Image dimensions
+ * @param fname     File name
+ */
+void write_pgm( uint8_t *image, const int dims[], const char * fname ) {
+    
+    FILE *f = fopen(fname, "wb");
+    
+    // Write PGM header
+    fprintf(f, "P5\n%d %d\n255\n", dims[0], dims[1]);
+    // Write image data
+    fwrite(image, 1, dims[0] * dims[1], f);
+    
+    fclose(f);
+}
+
+/**
+ * @brief Write CSV data file
+ * 
+ * @param data      1D vector data
+ * @param n         Number of elements
+ * @param fname     File name
+ */
+void write_csv( int data[], const int n, const char * fname ) {
+    FILE *f = fopen( fname, "w" );
+    fprintf(f, "level, count\n");
+    for( int i = 0; i < n; i++ ) {
+        fprintf(f,"%d,%d\n", i, data[i]);
+    }
+    fclose(f);
+}
+
+
+/**
+ * @brief Apply gaussian blur filter to image
+ * 
+ * @param img   Image data (input / output)
+ * @param nx    Image x dimension
+ * @param ny    Image inner y dimension, actual buffer is expected to
+ *              have ny + y cells
+ */
+void gauss( uint8_t *img, const int nx, const int ny ) {
+    /**
+     * @brief Gaussian blur kernel
+     * 
+     */
+    float kernel[3][3] = {
+        { 0.0625, 0.125, 0.0625},
+        { 0.125,  0.25,  0.125},
+        { 0.0625, 0.125, 0.0625}
+    };
+
+    uint8_t * tmp = malloc( nx * (ny + 2) );
+
+    // Apply kernel convolution and store in tmp
+    for( int j = 1; j < ny+1; j++ ) {
+        for( int i = 1; i < nx-1; i++ ) {
+            float f = 0.;
+            for( int k1 = -1; k1 <= 1; k1 ++ )
+                for( int k0 = -1; k0 <= 1; k0 ++ ) {
+                    f += img[ (j + k1) * nx + (i+k0) ] * kernel[k1+1][k0+1];
+                }
+            tmp[ j * nx + i ] = f;
+        }
+    }
+
+    // Copy result back
+    for( int j = 1; j < ny+1; j++ ) {
+        for( int i = 1; i < nx-1; i++ ) {
+            img[ j*nx + i ] = tmp[ j * nx + i ];
+        }
+    }
+    
+    free( tmp );
+}
+
+/**
+ * @brief Compute image histogram
+ * 
+ * The histogram counts how many pixels of a given intensity show up on the
+ * image
+ * 
+ * @param histogram     Histogram data
+ * @param img           Image data
+ * @param nx    Image x dimension
+ * @param ny    Image inner y dimension, actual buffer is expected to
+ *              have ny + y cells
+ */
+void hist( int histogram[], uint8_t * restrict img, const int nx, const int ny ) {
+
+    // Set all histogram values to 0
+    for( int i = 0; i < 256; i++ ) 
+        histogram[i] = 0;
+
+    // Compute histogram
+    for( int j = 1; j < ny+1; j++ )
+        for( int i = 1; i < nx-1; i++) {
+            int value = img[ i + nx*j];
+            histogram[ value ] ++;
+        }
+}
+
+int main( int argc, char * argv[]) {
+    /**
+     * @brief Number of passes
+     * 
+     */
+    const int level = 32;
+
+    MPI_Init( &argc, &argv );
+
+    int size, rank;
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file.pgm>\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    // Image dimensions
+    int dims[2];
+    uint8_t * img;
+
+    if ( rank == 0 ) {
+        int err = read_pgm( &img, dims, argv[1] );
+        if ( err ) {
+            fprintf(stderr, "Error reading file: %s\n", argv[1]);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        if ( dims[1] % size != 0 ) {
+            fprintf(stderr, "Image y-dimension (%d) does not divide evenly by number of processes (%d)\n",
+                     dims[1], size);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+
+    // (*todo*) 1. Broadcast image dimensions (dims) to all processes
+    // ...
+
+    int nx = dims[0];
+    int ny = dims[1] / size;
+    uint8_t * local_img = malloc( nx  * ( ny + 2 ) );
+
+    // (*todo*) 2. Split the image data over all processes
+    // ...
+
+    // Compute histogram
+    int histogram[256];
+    hist( histogram, local_img, nx, ny );
+
+    int global[256];
+    
+    // (*todo*) 4. Compute the global histogram
+    // ...
+    
+    if ( rank == 0 ) {
+        write_csv( global, 256, "histogram.csv");
+    }
+
+    // Apply image filter
+    for( int i = 0; i < level; i++ ) {
+        MPI_Request lower[2], upper[2];
+
+        // (*todo*) 5. Exchange edge values
+        // ...
+
+        // Filter
+        gauss( local_img, nx, ny );
+    }
+
+    // (*todo*) 3. Gather the image from all processes on rank 0
+    // ...
+
+    free( local_img );
+
+    // Save result
+    if ( rank == 0 ) {
+        write_pgm( img, dims, "filter.pgm");
+        free( img );
+    }
+
+    MPI_Finalize();
+    return 0;
+}
